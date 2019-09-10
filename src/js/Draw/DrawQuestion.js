5fe08f5c0664d4a69a7d2617d32c194cc523b7dc
BartoszB <bartosz.burdalski@gmail.com>
2019-09-10 23:16:49 +0200

    Ended DrawQuestion class

--- src/js/Draw/DrawQuestion.js
+++ src/js/Draw/DrawQuestion.js
@@ -2,6 +2,9 @@ import Draw from './Draw';
 
 export default class DrawQuestion extends Draw {
     constructor() {
-
+        super();
+    }
+    draw(length) {
+        return this.drawIndex(length);
     }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,10 @@ import category from './Data/Category';
 import qa from './Data/QuestionsAndAnswers';
 import App from './App.js';
 console.log(category.length);
-console.log(Object.keys(qa)[2]);
+// console.log(Object.keys(qa)[2]);
+const { Muzyka: [{ question }] } = qa;
+const { Książki } = qa;
+console.log(Książki)
+console.log(question)
 //Init game
 // const app = new App();
