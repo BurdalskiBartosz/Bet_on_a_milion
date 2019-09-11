1de9e568bdc61745f8d19249118436d47d326248
BartoszB <bartosz.burdalski@gmail.com>
2019-09-11 22:50:16 +0200

    Simple version of Bet class

--- src/js/Logic/Bet.js
+++ src/js/Logic/Bet.js
@@ -1,5 +1,20 @@
 export default class Bet {
     constructor() {
-
+    }
+    bet(key, money) {
+        switch (key) {
+            case 38:
+                return money == 0 ? event.preventDefault() : money -= 50000;
+            case 40:
+                return money == this.yourMoneyToBet ? event.preventDefault() : this.checkYouCanStillPlay(e.target.value);
+            default:
+                event.preventDefault();
+                alert("ZŁY PRZYCISK");
+        }
+    }
+    checkYouCanStillPlay(e) {
+        if (e != 0) {
+            money = parseInt(money) + 50000;
+        }
     }
 }
