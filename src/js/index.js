002aab5671e021338b65cd6914cc51ad23a7207c
BartoszB <bartosz.burdalski@gmail.com>
2019-09-11 22:11:57 +0200

    Base structure

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,3 @@ import '../scss/style.scss';
 import category from './Data/Category';
 import qa from './Data/QuestionsAndAnswers';
 import App from './App.js';
-console.log(category.length);
-// console.log(Object.keys(qa)[2]);
-const { Muzyka: [{ question }] } = qa;
-const { Książki } = qa;
-console.log(Książki)
-console.log(question)
-//Init game
-// const app = new App();
