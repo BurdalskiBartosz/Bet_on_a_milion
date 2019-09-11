654302127386f8024d19220e29eaf5b41d37313b
BartoszB <bartosz.burdalski@gmail.com>
2019-09-11 22:23:39 +0200

    Added Bet class

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,3 @@ import '../scss/style.scss';
 import category from './Data/Category';
 import qa from './Data/QuestionsAndAnswers';
 import App from './App.js';
-
-
-import Timer from './Logic/Timer';
-const timer = new Timer(13);
-const div = document.querySelector('#root');
-timer.countDown(div)
