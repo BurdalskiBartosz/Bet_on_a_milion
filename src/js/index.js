a0e5bd1f0b03e2b516f2967d5315d9e0901dee3b
BartoszB <bartosz.burdalski@gmail.com>
2019-09-11 22:09:28 +0200

    Added Timer class

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,9 @@ import '../scss/style.scss';
 import category from './Data/Category';
 import qa from './Data/QuestionsAndAnswers';
 import App from './App.js';
-console.log(category.length);
-// console.log(Object.keys(qa)[2]);
-const { Muzyka: [{ question }] } = qa;
-const { Książki } = qa;
-console.log(Książki)
-console.log(question)
-//Init game
-// const app = new App();
+
+
+import Timer from './Logic/Timer';
+const timer = new Timer(13);
+const div = document.querySelector('#root');
+timer.countDown(div)
