8a2e05e8fc301613ff2f36f1e366ce32409111ec
BartoszB <bartosz.burdalski@gmail.com>
2019-10-29 21:48:22 +0100

    Chenged

--- src/js/Draw/DrawCategory.js
+++ src/js/Draw/DrawCategory.js
@@ -9,15 +9,9 @@ export default class DrawCategory extends Draw {
     draw() {
         const drawnCategories = new Array();
         let counter = 0;
-        do {
-            const drawnIndex = this.drawIndex(this.category.length);
-            let drawnCategory = this.category[drawnIndex];
-            if (drawnCategories.includes(drawnCategory) != true && this.usedCategories.includes(drawnCategory) != true) {
-                drawnCategories.push(drawnCategory);
-                counter++;
-            }
-        } while (counter != 2);
-        return drawnCategories;
+        // NOWA LINIJKA
+
+        const variable = "WARTOSC";
     }
     excludeCategory(category) {
         this.usedCategories.push(category);
